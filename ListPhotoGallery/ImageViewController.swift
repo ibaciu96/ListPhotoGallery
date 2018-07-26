9af4609c50eca4d4533dc3811b770c7ff0b1ca01
Baciu Ionut <ionut.b096@yahoo.com>
2018-07-26 15:46:50 +0300

    cache and new view added

---
+++ ListPhotoGallery/ImageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ImageViewController.swift
+//  ListPhotoGallery
+//
+//  Created by Ionut Baciu on 26/07/2018.
+//  Copyright © 2018 Ionut Baciu. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
